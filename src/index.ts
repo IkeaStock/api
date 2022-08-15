964a420e6c3b87d2bd3a0eac3ee9f3809a043fa9
Sean Outram <outramsean0@gmail.com>
2022-08-15 14:40:08 +0000

    initial docs

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,11 @@ import { internal } from "./internal";
 import shapeshiftConstants from "./shapeshiftConstants";
 export class IkeaApi {
     public async get(countryCode: string, itemCode: string) {
+        /**
+         * Gets the stock numbers of an item from the Ikea API
+         * @param countryCode - the country code of the country the item is in
+         * @param itemCode - the item code of the item to get the stock numbers of
+         */
         const parsedItemCode = Number(itemCode.replaceAll(".", ""))
         return await internal.getById(shapeshiftConstants.string(countryCode), shapeshiftConstants.number(parsedItemCode))
     }
--- src/shapeshiftConstants.ts
+++ src/shapeshiftConstants.ts
@@ -2,9 +2,19 @@ import { s } from "@sapphire/shapeshift"
 
 class shapeshiftConstants {
     public string(string: string) {
+        /**
+         * Validates Strings
+         * @param string - the string to 
+         * @private
+         */
         return s.string.parse(string)
     }
     public number(number: number) {
+        /**
+         * Validates Numbers
+         * @param number - the number to validate
+         * @private
+        */
         return s.number.parse(number)
     }
 }
